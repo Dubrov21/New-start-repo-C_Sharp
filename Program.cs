02e1eb0275e5937770dc7954206c09d8c5b0f297
Dubrov21 <vald5red@yandex.ru>
2023-09-21 19:40:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Начальный_проект
+{
+    class Program
+    {
+        static void Main(string[] args)//Для закпуска нажать Ctrl + F5
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
